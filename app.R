8f817ad3e5c6ec9d7daa5b6855a4cde754789e65
Andrew Simpkin <andrew.simpkin@orreco.com>
2020-03-15 10:22:37 +0000

    updated app and data to allow country comparisons

--- app.R
+++ app.R
@@ -60,12 +60,9 @@ sidebar <- dashboardSidebar(disable=FALSE,
                             
                             #### ####
                             
-                            tags$a(href='https://www.nuigalway.ie/science/school-of-maths/',
-                                           tags$img(src='logo.jpg',height='50',width='150')),
-                            
-
                             sidebarMenu(id = 'tabs', 
                                         menuItem("Time", icon = icon("line-chart"), tabName = "time"),
+                                        menuItem("Compare", icon = icon("globe"), tabName = "compare"),
                                         menuItem("Map", icon = icon("map"), tabName = "map"),
                                         menuItem("Data", icon = icon("eye"), tabName = "data")
                             )
@@ -84,9 +81,9 @@ body <- dashboardBody(
         # Map Tab #
         tabItem("map",    
                 fluidRow(
-                  box(title = "Map using Belfast, Cork, Dublin and Galway Hospitals as locations (location annoncement stopped on March 12th)", 
+                  box(title = "Map using Belfast, Cork, Dublin, Letterkenny and Galway Hospitals as locations (no locations for 47 cases announced March 12/13th)", 
                       width = 12,
-                      leafletOutput("map", width = "70%", height = 750)
+                      leafletOutput("map", width = "70%", height = 600)
                     )
                   )
 
@@ -102,8 +99,27 @@ body <- dashboardBody(
                   ),            
                 fluidRow(
                     box(width = 12, title = "Cumulative and new cases per day", 
-                        plotlyOutput("cumulcases", width = "70%", height = 500)
+                        plotlyOutput("cumulcases", width = "80%", height = 400)
                     )
+                )
+        ),
+        
+        # Country comparison Tab #
+        tabItem("compare", 
+                
+                fluidRow(
+                  column(width=6,
+                         selectInput("place", "Country to compare", 
+                                     choices = c("france", "germany", "italy", "spain", "uk"), 
+                                     selected = "italy")),
+                  column(width=6,
+                         sliderInput("days","Decide the time difference",min = -5,max=30,value=13)
+                  )
+                ),
+                fluidRow(
+                  box(width = 12, title = "Compare Ireland with other countries scaled to our population, with time shifted", 
+                      plotlyOutput("irelandcompare", width = "90%", height = 500)
+                  )
                 )
         ),
         
@@ -123,22 +139,34 @@ body <- dashboardBody(
 # Define server logic required to draw a leaflet
 server <- function(input, output) {
     
-    # read in and clean data
+    # read in all data
     dataRaw <- reactive({
       read.csv("data/corona_island.csv")
     })
     
-    # read in and clean data
+    # irish data only
+    dataIreland <- reactive({
+      dataRaw() %>% filter(country == "ireland")
+    })
+
+    # Country comparison data
+    dataCountry <- reactive({
+      dataRaw() %>% filter(country=="ireland" | country == input$place)
+    })
+    
+    # irish area data
     dataArea <- reactive({        
-    dataRaw() %>% 
+    dataIreland() %>% 
             mutate(long = case_when(area=="east" ~ -6.2690,
                                     area=="west" ~ -9.0650,
                                     area=="south" ~ -8.5110,
-                                    area=="north" ~ -5.954),
+                                    area=="north" ~ -5.954,
+                                    area=="northwest" ~ -7.7328),
                    lat = case_when(area=="east" ~ 53.3593,
                                    area=="west" ~ 53.2760,
                                    area=="south" ~ 51.8830,
-                                   area=="north" ~ 54.588))  %>% 
+                                   area=="north" ~ 54.588,
+                                   area=="northwest" ~ 54.9598))  %>% 
         group_by(area) %>% summarise(ncases=sum(ncase,na.rm=TRUE),
                                              ndeaths=sum(ndeath,na.rm=TRUE),
                                              long = mean(long,na.rm=TRUE),
@@ -146,6 +174,7 @@ server <- function(input, output) {
             na.omit() 
     })
     
+ 
     ## Interactive Map ###########################################
     
     # Create the map
@@ -156,12 +185,12 @@ server <- function(input, output) {
                 urlTemplate = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
                 attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
             ) %>%
-            setView(lng = -8, lat = 53, zoom = 7) %>%
+            setView(lng = -8, lat = 53.5, zoom = 7) %>%
             clearShapes() %>%
             addCircleMarkers(lng= ~long, 
                              lat= ~lat, 
                              layerId = ~area,
-                             radius = ~ncases*3)
+                             radius = ~ncases*1.5)
     })
     
     # Show a popup at the given location
@@ -188,10 +217,10 @@ server <- function(input, output) {
         })
     })
     
-    
+    ################### Info boxes ##########
     ## Info box
     output$CasesBox <- renderInfoBox({
-      dat <- dataRaw()
+      dat <- dataIreland()
       infoBox(
         "Number of cases", paste0(sum(dat$ncase,na.rm=TRUE)), icon = icon("list"),
         color = "purple"
@@ -200,7 +229,7 @@ server <- function(input, output) {
     
     ## mort box
     output$MortBox <- renderInfoBox({
-      dat <- dataRaw()
+      dat <- dataIreland()
       infoBox(
         "Number of deaths", paste0(sum(dat$ndeath,na.rm=TRUE)), icon = icon("list"),
         color = "red"
@@ -210,25 +239,27 @@ server <- function(input, output) {
     
     ## mort box
     output$RecovBox <- renderInfoBox({
-      dat <- dataRaw()
+      dat <- dataIreland()
       infoBox(
         "Recovered", paste0(sum(dat$nrecov,na.rm=TRUE)), icon = icon("list"),
         color = "green"
       )
     })
     
-    #### Data Table
+    
+    
+    #################### Data Table  ###################
     output$dattable <- renderDT({
       
-      dataRaw() %>% select(-age)
+      dataRaw() %>% mutate(date = as.Date(date,format = "%d/%m/%Y"))
     
       })
     
-    
+    ################### Time series plots ##############
     #### Plotly cases per day
     output$cumulcases <- renderPlotly({
       
-      g = dataRaw() %>%
+      g = dataIreland() %>%
         mutate(date = as.Date(date,format = "%d/%m/%Y")) %>%
         group_by(date) %>%
         summarise(ncases = sum(ncase), New_cases = sum(ncase), Date = min(date)) %>%
@@ -241,20 +272,45 @@ server <- function(input, output) {
       
     })
     
-    #### Plotly cases per day
-    output$timeseries <- renderPlotly({
-  
-            g = dataRaw() %>%
-              mutate(date = as.Date(date,format = "%d/%m/%Y")) %>%
-              group_by(date) %>%
-              summarise(ncases = sum(ncase)) %>%
-              na.omit() %>%
-              ggplot(aes(x=date,y=ncases)) + 
-              geom_line() + geom_point() + theme(legend.position="none") + labs(y="New cases")
-        ggplotly(g)
-        
-    })
+    # #### Plotly cases per day
+    # output$timeseries <- renderPlotly({
+    # 
+    #         g = dataIreland() %>%
+    #           mutate(date = as.Date(date,format = "%d/%m/%Y")) %>%
+    #           group_by(date) %>%
+    #           summarise(ncases = sum(ncase)) %>%
+    #           na.omit() %>%
+    #           ggplot(aes(x=date,y=ncases)) + 
+    #           geom_line() + geom_point() + theme(legend.position="none") + labs(y="New cases")
+    #     ggplotly(g)
+    #     
+    # })
     
+
+    ################### Comparison plots ############
+    ##### Plotly Ireland v other countries
+    output$irelandcompare <- renderPlotly({
+      
+      
+      dat <- dataCountry()
+      myorder <- c("ireland","france","germany","italy","spain","uk")
+      dat <- dat %>% mutate(date = as.Date(date,format = "%d/%m/%Y"), country = as.character(country)) 
+      dat$country <- dat$country[order(match(dat$country,myorder))]
+      prop = mean(dat$pop[dat$country=="ireland"])/mean(dat$pop[dat$country==input$place])
+      dat$ncase[dat$country==input$place] <- round(dat$ncase[dat$country==input$place]*prop,0)
+      dat$date[dat$country==input$place] <- dat$date[dat$country==input$place]+input$days
+      dat$country[dat$country==input$place] <- paste(input$place, "scaled to ireland +",input$days,"days")
+      
+      g = dat %>% mutate(date = as.Date(date,format = "%d/%m/%Y"))%>%
+        group_by(country, date) %>%
+        summarise(ncases = sum(ncase), New_cases = sum(ncase), Date = min(date), Country = country[1]) %>%
+        na.omit() %>%
+        mutate(ccases = cumsum(ncases), Total_cases= cumsum(ncases)) %>%
+        ggplot(aes(x=date,y=ccases,color=country,fill=country,label=Date,label1=Country,label2=Total_cases)) +
+        geom_line() + geom_point() + labs(y="Cases") + theme_bw()
+      ggplotly(g, tooltip = c("Country","Date","Total_cases","New_cases"))
+      
+    })    
     
 }
 
